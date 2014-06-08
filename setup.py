8ce16c5264b875ac5272c5988304b0f4eea748b5
Борисов Кирилл <borisov@bars-open.ru>
2014-06-08 19:35:36 +0400

    Увеличена версия.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3-fias',
-      version='0.1.0.0',
+      version='0.1.1.0',
       url='https://src.bars-open.ru/py/m3/m3_contrib/kladr',
       license='Apache License, Version 2.0',
       author='BARS Group',
