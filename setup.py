bf8db9b4a82880ab7509bc0d37a822c68e803574
Борисов Кирилл <borisov@bars-open.ru>
2014-06-08 14:19:56 +0400

    Исправлена ошибка в setup.py, добавлен список зависимостей.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from setuptools import setup, find_packages
 
 requires = []
-with open('src/requires.txt', 'r') as f:
+with open('requirements.txt', 'r') as f:
     requires.extend(f.readlines())
 
 setup(name='m3-fias',
-      version='0.1.0.0',x
+      version='0.1.0.0',
       url='https://src.bars-open.ru/py/m3/m3_contrib/kladr',
       license='Apache License, Version 2.0',
       author='BARS Group',
