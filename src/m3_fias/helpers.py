4f4b753d34d1b194c0e2393e9a9e2b275750b390
chagochkin <devnull@localhost>
2014-08-07 12:05:27 +0700

    Функция helpers.get_ao_object переведена на использование FiasAddressObject

--- src/m3_fias/helpers.py
+++ src/m3_fias/helpers.py
@@ -20,27 +20,21 @@ class FiasServerError(IOError):
 
 
 def get_ao_object(guid):
-    uuid.UUID(guid)  # проверка, GUID это или нет
-
-    request_path = '{0}/objects/ao/{1}'.format(settings.FIAS_API_URL, guid)
-    response = requests.get(request_path, params={'trust_env': False})
-
-    if response.status_code == 404:
-        # объект с указанным GUID не найден в ФИАС
-        return None
-    elif response.status_code == 200:
-        obj = response.json()
-
-        return {
-            'ao_guid': obj['guid'],
-            'ao_level': obj['level'],
-            'address': obj['address'],
-            'shortname': obj['short_name'],
-            'formal_name': obj['formal_name'],
-            'name': u'{0}. {1}'.format(obj['short_name'], obj['formal_name'])
+    address_object = FiasAddressObject.create(guid)
+    result = {}
+    if address_object is not None:
+        name = u'{0}. {1}'.format(address_object.short_name,
+                                  address_object.formal_name)
+        result = {
+            'ao_guid': address_object.guid,
+            'ao_level': address_object.level,
+            'address': address_object.address,
+            'shortname': address_object.short_name,
+            'formal_name': address_object.formal_name,
+            'name': name,
         }
-    else:
-        raise FiasServerError(response=response)
+
+    return result
 
 
 def kladr2fias(code, generate_error=False):
