a62c1dbe0b1e96e9d10e654e9e712ab36a99f080
Максим Елахов <elakhov@bars-open.ru>
2015-03-27 14:04:27 +0400

    0.2.0.8

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-#coding: utf-8
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 requires = []
 with open('src/requirements.txt', 'r') as f:
     requires.extend(f.readlines())
 
-setup(name='m3-fias',
-      version='0.2.0.7',
-      url='https://bitbucket.org/barsgroup/m3-fias',
-      license='MIT',
-      author='BARS Group',
-      description=u'Федеральная информационная адресная система',
-      author_email='bars@bars-open.ru',
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      install_requires=requires,
-      include_package_data=True,
-      classifiers=[
+setup(
+    name='m3-fias',
+    version='0.2.0.8',
+    url='https://bitbucket.org/barsgroup/m3-fias',
+    license='MIT',
+    author='BARS Group',
+    description=u'Федеральная информационная адресная система',
+    author_email='bars@bars-open.ru',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=requires,
+    include_package_data=True,
+    classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Natural Language :: Russian',
@@ -25,5 +26,5 @@ setup(name='m3-fias',
         'Programming Language :: Python',
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
-      ],
+    ],
 )
