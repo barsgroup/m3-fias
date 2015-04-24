ce02672179822e7fb0fd959918a01b67f23b5171
Максим Елахов <elakhov@bars-open.ru>
2015-04-24 11:49:23 +0400

    0.2.0.11

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('src/requirements.txt', 'r') as f:
 
 setup(
     name='m3-fias',
-    version='0.2.0.10',
+    version='0.2.0.11',
     url='https://bitbucket.org/barsgroup/m3-fias',
     license='MIT',
     author='BARS Group',
