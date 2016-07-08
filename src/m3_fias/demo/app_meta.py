540227f3df6630dd3347e163df52fce96760ce68
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2016-07-08 09:28:23 +0600

    EDUADNL-1351 Корректировка импортов из objectpack.
    При добавлении в objectpack поддержки Django 1.9+ появилась проблема
    инициализации системы, вызванная наличием импортов в
    objectpack/__init__.py. В связи с этим импорты были удалены из этого
    модуля, что, в свою очередь, потребовало корректировки импортов в
    других пакетах.

--- src/m3_fias/demo/app_meta.py
+++ src/m3_fias/demo/app_meta.py
@@ -1,11 +1,8 @@
-#coding: utf-8
-
-from m3.actions import ActionController, ControllerCache
+# coding: utf-8
+from m3.actions import ActionController
+from objectpack.desktop import uificate_the_controller
 
 from m3_fias.demo.actions import FiasDemoPack
-
-from m3_ext.ui.app_ui import add_desktop_launcher
-from objectpack import uificate_the_controller
 
 
 fias_controller = ActionController(url='/fias')
@@ -16,10 +13,6 @@ def register_actions():
         FiasDemoPack()
         ])
 
+
 def register_desktop_menu():
     uificate_the_controller(fias_controller)
-	# pack = ControllerCache.find_pack(FiasDemoPack)
-	# add_desktop_launcher(u'Адресный компонент ФИАС', 
-	# 					 metaroles=('none'),
-	# 					 places=(1),
-	# 					 url=pack.window_action.get_absolute_url())
