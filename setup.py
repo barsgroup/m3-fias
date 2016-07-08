fcabc04b0bf771ebeb053d6b4361c43653bb4356
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2016-07-08 10:35:52 +0600

    Подключение m3-builder для сборки дистрибутива.

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
+import os
+
+from pip.download import PipSession
+from pip.req.req_file import parse_requirements
 from setuptools import setup, find_packages
 
-requires = []
-with open('src/requirements.txt', 'r') as f:
-    requires.extend(f.readlines())
+
+def _get_requirements(file_name):
+    pip_session = PipSession()
+    requirements = parse_requirements(file_name, session=pip_session)
+
+    return tuple(str(requirement.req) for requirement in requirements)
+
+
+def _read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__),
+            fname)).read()
+    except IOError:
+        return ''
+
 
 setup(
     name='m3-fias',
-    version='0.2.0.16',
     url='https://bitbucket.org/barsgroup/m3-fias',
     license='MIT',
     author='BARS Group',
@@ -15,7 +30,8 @@ setup(
     author_email='bars@bars-open.ru',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    install_requires=requires,
+    install_requires=_get_requirements('requirements.txt'),
+    long_description=_read('README.rst'),
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
@@ -27,4 +43,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
     ],
+    dependency_links=(
+        'http://pypi.bars-open.ru/simple/m3-builder',
+    ),
+    setup_requires=(
+        'm3-builder>=1.0.1',
+    ),
+    set_build_info=os.path.dirname(__file__),
 )
