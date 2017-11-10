f6aa8dd4ff01806778bca1e4b35d7231c07d3a86
Андрей Чагочкин <andrey.chagochkin@bars-open.ru>
2017-11-10 21:03:34 +0700

    EDUM3-11 Реогранизации инфраструктуры дистрибутива.
    1. Списки зависимостей разделены и перенесены в отдельную папку.
    2. Актуализировано описание пакета в README.rst.
    3. Выполнены мелкие корректировки в setup.py.

--- setup.py
+++ setup.py
@@ -1,52 +1,60 @@
 # coding: utf-8
-import os
+from os import chdir
+from os.path import abspath
+from os.path import dirname
+from os.path import isabs
+from os.path import join
+from os.path import relpath
 
 from pip.download import PipSession
 from pip.req.req_file import parse_requirements
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from setuptools import setup
 
 
-def _get_requirements(file_name):
+def _get_requirements(file_path):
     pip_session = PipSession()
-    requirements = parse_requirements(file_name, session=pip_session)
+    requirements = parse_requirements(file_path, session=pip_session)
 
     return tuple(str(requirement.req) for requirement in requirements)
 
 
-def _read(fname):
-    try:
-        return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    except IOError:
-        return ''
+def _read(file_path):
+    with file(file_path, 'r') as infile:
+        return infile.read()
 
 
 setup(
     name='m3-fias',
-    url='https://bitbucket.org/barsgroup/m3-fias',
+    url='https://github.com/barsgroup/m3-fias',
     license='MIT',
     author='BARS Group',
     description=u'Федеральная информационная адресная система',
     author_email='bars@bars-open.ru',
     package_dir={'': 'src'},
     packages=find_packages('src'),
-    install_requires=_get_requirements('requirements.txt'),
+    install_requires=_get_requirements('requirements/prod.txt'),
     long_description=_read('README.rst'),
     include_package_data=True,
     classifiers=[
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Natural Language :: Russian',
-        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'License :: OSI Approved :: MIT License',
         'Development Status :: 5 - Production/Stable',
     ],
+    extras_require={
+        'oauth2': (
+            'oauthlib>=2,<3',
+            'requests-oauthlib<1',
+        ),
+    },
     dependency_links=(
         'http://pypi.bars-open.ru/simple/m3-builder',
     ),
     setup_requires=(
         'm3-builder>=1.0.1',
     ),
-    set_build_info=os.path.dirname(__file__),
+    set_build_info=dirname(__file__),
 )
